50f93a57208fe37a3d725e9a6fea9ea3555be3af
Prasadnair <prasadnair44@gmail.com>
2024-03-09 23:06:34 -0500

    minimal API with Carter

---
+++ MinimalAPIWithCarter/MinimalAPIWithCarter/Modules/SampleModule.cs
@@ -0,0 +1,18 @@
+﻿using Carter;
+
+namespace MinimalAPIWithCarter.Modules
+{
+    public class SampleModule : CarterModule
+    {
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+
+            app.MapGet("/hello", () =>
+            {
+                return "Hello, World!";
+                
+            });
+
+        }
+    }
+}
